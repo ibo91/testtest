c7443059276674215d46c84cc213ead5ce1b7ff6
Prelang Builder <builder@prelang.com>
2017-03-21 17:28:35 +0000

    Installed relation 'belongs_to' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
   has_many :karteikartes
+  belongs_to :karteikarte
 end
